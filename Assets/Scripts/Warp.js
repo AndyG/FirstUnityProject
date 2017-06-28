5002dc0ee368d2ea1d662d77f73dc0f99d69991e
angarron <anderson.garron@gmail.com>
2017-06-28 00:02:10 -0700

    Added a spinning coin and some warp pads!

---
+++ Assets/Scripts/Warp.js
@@ -0,0 +1,14 @@
+﻿var warptarget: Transform;
+
+function Update() {
+
+}
+
+function OnTriggerEnter(col: Collider) {
+	if (col.gameObject.tag == "warp1") {
+		Debug.Log("warp script called");
+		this.transform.position = warptarget.position;
+	} else if(col.gameObject.tag == "warp2") {
+	
+	}
+}
