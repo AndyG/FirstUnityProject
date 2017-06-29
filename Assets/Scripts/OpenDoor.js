8c3dc70d213413907d7736bf1a1cc1d8cbd720af
andygarron <anderson.garron@gmail.com>
2017-06-28 19:26:30 -0700

    Added a door that can be opened and closed

---
+++ Assets/Scripts/OpenDoor.js
@@ -0,0 +1,9 @@
+﻿function OnMouseDown() {
+
+		this.transform.parent.gameObject.transform.Rotate(0, 90, 0, Space.World);
+	}
+
+
+function OnMouseUp() {
+		this.transform.parent.gameObject.transform.Rotate(0, 270, 0, Space.World);
+	}
