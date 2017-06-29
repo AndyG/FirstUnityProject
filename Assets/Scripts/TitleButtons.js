cc5755eeff97d6f3bf3125af24f8f091b2b1f17e
angarron <anderson.garron@gmail.com>
2017-06-29 01:08:02 -0700

    Completed the game, basically

---
+++ Assets/Scripts/TitleButtons.js
@@ -0,0 +1,11 @@
+﻿import UnityEngine.SceneManagement;
+
+function OnGUI() {
+	if (GUI.Button (Rect (Screen.width/2-50, Screen.height/2, 100, 30), "Git Gud")) {
+		SceneManager.LoadScene("WallsOnGround");
+	}
+
+	if (GUI.Button (Rect (Screen.width/2-50, Screen.height/2 + 40, 100, 30), "Git Out")) {
+		Application.Quit();
+	}
+}
